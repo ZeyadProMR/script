0b3de0148c316d2910718cf9d06fa4c2162375da
ZeyadProMR <97927937+ZeyadProMR@users.noreply.github.com>
2022-08-27 23:52:21 +0300

    Update doorscut.lua

--- doorscut.lua
+++ doorscut.lua
@@ -1,1 +1,6 @@
-
+if game.PlaceID == 6516141723 then
+   loadstring(game:HttpGet(("https://raw.githubusercontent.com/ZeyadProMR/script/main/DOORSLOBBY.lua")))()
+end
+if game.PlaceID == 6839171747 then
+   loadstring(game:HttpGet(("https://github.com/ZeyadProMR/script/blob/main/DOORS.lua")))()
+end
