8ac176483c62aa5aa85662990b669f6dffef2d85
ZeyadProMR <97927937+ZeyadProMR@users.noreply.github.com>
2022-08-28 00:09:01 +0300

    Delete doorscut.lua

--- doorscut.lua
+++
@@ -1,6 +0,0 @@
-if game.PlaceID == 6516141723 then
-   loadstring(game:HttpGet(("https://raw.githubusercontent.com/ZeyadProMR/script/main/DOORSLOBBY.lua")))()
-end
-if game.PlaceID == 6839171747 then
-   loadstring(game:HttpGet(("https://raw.githubusercontent.com/ZeyadProMR/script/main/DOORS.lua")))()
-end
